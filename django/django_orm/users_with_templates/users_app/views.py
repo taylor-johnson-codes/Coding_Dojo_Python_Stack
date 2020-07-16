b0a750f8d578acf675efc01545e695ab51a0c9ef
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-07-15 19:12:49 -0700

    working on assignment

--- django/django_orm/users_with_templates/users_app/views.py
+++ django/django_orm/users_with_templates/users_app/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render, redirect
 from .models import User
 
 def index(request):
-    return render(request, "index.html")
+    request.session['fname'] = None
+    request.session['lname'] = None
+    request.session['email'] = None
+    request.session['age'] = None
+    all_users_from_DB = User.objects.all()
+    context = {
+        "all_users":all_users_from_DB
+    }
+    return render(request, "index.html", context)
 
-def add(request):
+def add_user(request):
     postFirstName = request.POST['fname']
     postLastName = request.POST['lname']
     postEmail = request.POST['email']
     postAge = request.POST['age']
-    User.objects.create(first_name=postFirstName, last_name=postLastName, email=postEmail, age=postAge)
+    User.objects.create(first_name=postFirstName, last_name=postLastName, email_address=postEmail, age=postAge)
     return redirect("/")
