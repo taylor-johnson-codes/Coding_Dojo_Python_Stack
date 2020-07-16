2cc5694ca4c1ede0962cc64d74cbd5ba6d55f19d
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-07-15 21:43:36 -0700

    initial commit; assignment is finished

---
+++ django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # ninjas = list of ninjas associated with a given Dojo location
+    desc = models.TextField()
+    def __str__(self):
+        return f'{self.name} in {self.city}, {self.state}'
+
+class Ninjas(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(Dojos, related_name="ninjas", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'Ninja {self.first_name} {self.last_name}'
---
+++ django/django_orm/dojo_ninjas_proj/dojo_ninjas_proj/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('dojo_ninjas_app.urls'))
+]
