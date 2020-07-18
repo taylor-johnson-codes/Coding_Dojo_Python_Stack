7729fecb0b0af0a6c6a8797169ca9f2674b3d0bc
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-07-18 15:49:40 -0700

    working on assignments

---
+++ django/django_orm/books_authors_proj/main_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # authors = list of all authors related to book
+    def __str__(self):
+        return f'{self.title} {self.desc}'
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name='authors', null=True)
+    notes = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.notes}'
