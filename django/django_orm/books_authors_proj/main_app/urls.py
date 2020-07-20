dd33258b0b38d9e29cbeaf7b020be7e586b751b4
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-07-19 18:51:13 -0700

    finished assignment

--- django/django_orm/books_authors_proj/main_app/urls.py
+++ django/django_orm/books_authors_proj/main_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('create_book', views.create_book),
+    path('create_author', views.create_author),
+    path('addAuthorToBook', views.addAuthorToBook), # needed when sending back info thru post method to create a relationship
+    path('books/<int:book_id>', views.show_book),
+    path('authors/<int:author_id>', views.show_author),
 ]
--- django/django_orm/books_authors_proj/main_app/views.py
+++ django/django_orm/books_authors_proj/main_app/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 def index(request):
-    return render(request, 'index.html')
+    all_books = Book.objects.all()
+    all_authors = Author.objects.all()
+    context = {
+        "all_books" : all_books,
+        "all_authors" : all_authors
+    }
+    return render(request, 'index.html', context)
+
+def create_book(request):
+    new_book = Book.objects.create(title=request.POST['title'], desc=request.POST['desc'])
+    return redirect('/')
+
+def create_author(request):
+    new_author = Author.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], notes=request.POST['notes'])
+    return redirect('/')
+
+def show_book(request, book_id):
+    one_book = Book.objects.get(id=book_id)
+    all_authors = Author.objects.all()
+    context = {
+        'book' : one_book,
+        "all_authors" : all_authors
+    }
+    return render(request, 'show_book.html', context)
+
+def show_author(request, author_id):
+    if request.method == "GET":
+        one_author = Author.objects.get(id=author_id)
+        all_books = Book.objects.all()
+        context = {
+            'author' : one_author,
+            'all_books' : all_books
+        }
+        return render(request, 'show_author.html', context)
+    if request.method == "POST":
+        book_id = request.POST['book_id']
+        book = Book.objects.get(id=book_id)
+        author = Author.objects.get(id=author_id)
+        # creating relationship:
+        book.authors.add(author)  # author.books.add(book)
+        return redirect('/authors/' + str(author_id))
+
+def addAuthorToBook(request):
+    book_id = request.POST['book_id']
+    author_id = request.POST['author_id']
+    book = Book.objects.get(id = book_id)
+    author = Author.objects.get(id = author_id)
+    # creating relationship:
+    book.authors.add(author)  # author.books.add(book)
+    return redirect('/books/' + str(book_id))
