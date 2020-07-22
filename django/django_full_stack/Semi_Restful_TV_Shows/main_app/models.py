c5222cc4786ec433b75e4dd7a37be0944f308b43
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-07-21 23:55:12 -0700

    added CSS and input validations

--- django/django_full_stack/Semi_Restful_TV_Shows/main_app/models.py
+++ django/django_full_stack/Semi_Restful_TV_Shows/main_app/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 
+# class with models.Manager inherits from ORM-buildin class and extends models' functionality
+class ShowManager(models.Manager):
+    # contains all built-in methods plus whatever methods we add here
+    def show_validator(self, postData):
+        errors = {}
+        # adds key/value to errors dict:
+        if len(postData['title']) < 2:
+            errors['title'] = "Title needs to be at least 2 characters."
+        # doesn't work on edit page:
+        # if Show.objects.filter(title = postData['title']):
+        #     errors['title'] = "Title is already in the database."
+        if len(postData['network']) < 3:
+            errors['network'] = "Network needs to be at least 3 characters."
+        if len(postData['desc']) < 10:
+            errors['desc'] = "Description needs to be at least 10 characters."
+        return errors
+
+# models.Model makes a database table
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -7,5 +25,6 @@ class Show(models.Model):
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()  # this overrides the original objects property
     def __str__(self):
         return f'{self.title} {self.network} {self.release_date} {self.desc}'
--- django/django_full_stack/Semi_Restful_TV_Shows/main_app/views.py
+++ django/django_full_stack/Semi_Restful_TV_Shows/main_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages  # need this for validations
 from .models import *
 
 def root(request):
     return redirect('/shows')
 
 def all_shows(request):
-    all_shows = Show.objects.all()
     context = {
-        'all_shows' : all_shows
+        'all_shows' : Show.objects.all()
     }
     return render(request, 'shows.html', context)
 
@@ -15,17 +15,26 @@ def new_show_page(request):
     return render(request, 'shows_new.html')
 
 def create(request):
-    title = request.POST['title']
-    network = request.POST['network']
-    release_date = request.POST['release_date']
-    desc = request.POST['desc']
-    new_show = Show.objects.create(title=title, network=network, release_date=release_date, desc=desc)
-    return redirect('/shows/' + str(new_show.id))
+    #1) validate post data:
+    errors = Show.objects.show_validator(request.POST)  # request.POST checks entire dict
+    # check if the errors dict contains any key/value pairs:
+    if len(errors) > 0:
+        # loop through each pair and make a flash message:
+        for key, value in errors.items():
+            messages.error(request, value)  # messages was imported at the top
+        return redirect('/shows/new')  # send user back to view to correct errors
+    #2) if validation passes, continue on:
+    else:
+        title = request.POST['title']
+        network = request.POST['network']
+        release_date = request.POST['release_date']
+        desc = request.POST['desc']
+        new_show = Show.objects.create(title=title, network=network, release_date=release_date, desc=desc)
+        return redirect('/shows/' + str(new_show.id))
 
 def shows_info(request, show_id):
-    show = Show.objects.get(id=show_id)
     context={
-        'show' : show
+        'show' : Show.objects.get(id=show_id)
     }
     return render(request, 'shows_info.html', context)
 
@@ -36,14 +45,19 @@ def edit(request, show_id):
     return render(request, 'shows_edit.html', context)
 
 def update(request, show_id):
-    show = Show.objects.get(id=show_id)
-    show.title = request.POST['title']
-    show.network = request.POST['network']
-    show.release_date = request.POST['release_date']
-    show.desc = request.POST['desc']
-    show.save()
-    return redirect('/shows/' + str(show_id))
-    
+    errors = Show.objects.show_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/' + str(show_id) + '/edit')
+    else:
+        show = Show.objects.get(id=show_id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.release_date = request.POST['release_date']
+        show.desc = request.POST['desc']
+        show.save()
+        return redirect('/shows/' + str(show_id))
 
 def destroy(request, show_id):
     show = Show.objects.get(id=show_id)
