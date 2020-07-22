e3e40c8d332c8d41905001ed9e6bde804a4785d2
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-07-22 11:43:58 -0700

    started new assignment (Login and Registration)

---
+++ django/django_full_stack/login_registration/main_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{self.} {self.}, {self.}'
