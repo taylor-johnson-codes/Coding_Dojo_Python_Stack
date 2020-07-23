7302056248a0bd025268c4be86a5f9a550b38113
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-07-22 20:58:15 -0700

    finished assignment

--- django/django_full_stack/login_registration/main_app/models.py
+++ django/django_full_stack/login_registration/main_app/models.py
@@ -1,13 +1,55 @@
 from django.db import models
+# import bcrypt
 
-# class User(models.Model):
-#     first_name = models.CharField(max_length=255)
-#     last_name = models.CharField(max_length=255)
-#     email = models.EmailField()
-#     password = models.CharField(max_length=255)
+class UserManager(models.Manager): 
+    def validate_registration(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2: # need letters only
+            errors['first_name'] = "First name needs to be at least 2 characters."
+        elif postData['first_name'].isalpha() == False:  # OR if not postData['first_name'].isalpha():
+            errors['first_name_alpha'] = "First name needs to be letters only."
+        if len(postData['last_name']) < 2: # need letters only
+            errors['last_name'] = "Last name needs to be at least 2 characters."
+        elif postData['last_name'].isalpha() == False:
+            errors['last_name_alpha'] = "Last name needs to be letters only."
+        if len(postData['email']) < 1:
+            errors['email'] = "Email is required."
+        elif User.objects.filter(email = postData['email']):  # or len() > 0  # Ninja bonus
+            errors['email_exists'] = "Email already exists in database."
+        if len(postData['password']) < 8:
+            errors['password'] = "Password needs to be at least 8 characters."
+        if postData['password'] != postData['confirm_password']:
+            errors['confirm'] = "Password and Confirm PW don't match."
+        return errors
 
+        # copy/paste into validations if not using EmailField() in models and input email in HTML:
+        # import re
+        # EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        # if not EMAIL_REGEX.match(postData['email']):
+        #     errors['email'] = "Invalid email format!"
 
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     def __str__(self):
-#         return f'{self.} {self.}, {self.}'
+    # tried to do validations in models.py, but couldn't get it to work because of errors2 coming back as NoneType
+    # def validate_login(self, postData):
+    #     errors2 = {}
+    #     user_list = User.objects.filter(email=postData['email'])
+    #     if len(postData['email']) < 1:
+    #         errors2['email'] = "Email is required."
+    #     elif User.objects.filter(email = postData['email']) == False:  # Ninja bonus
+    #         errors2['email_no_exist'] = "Email doesn't exists in database; please register."
+    #     elif len(user_list) > 0:  # meaning filter returned something in a list
+    #         logged_user = user_list[0]  # using first item in list (and hopefully only item) as our logged_user
+    #         password = logged_user.password
+    #         # pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    #         if bcrypt.checkpw(password.encode(), logged_user.password.encode()) == False:
+    #             errors2['wrong_password'] = "Wrong password."
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    objects = UserManager()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.email} {self.password}'
--- django/django_full_stack/login_registration/main_app/views.py
+++ django/django_full_stack/login_registration/main_app/views.py
@@ -1,8 +1,62 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
+import bcrypt
 
 def index(request):
     return render(request, 'index.html')
 
+def register(request):
+    errors = User.objects.validate_registration(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()  # create the hash
+        new_user = User.objects.create(first_name=first_name, last_name=last_name, email=email, password=pw_hash)
+        request.session['user_id'] = new_user.id  # to automatically login for the user instead of having them login on login page
+        return redirect('/success')
+
+def login(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    user_list = User.objects.filter(email=email)
+    if len(user_list) > 0:
+        logged_user = user_list[0]
+        if bcrypt.checkpw(password.encode(), logged_user.password.encode()):
+            request.session['user_id'] = logged_user.id
+            return redirect('/success')
+        else:
+            messages.error(request, "Invalid credentials.")
+            return redirect('/')
+    else:
+        messages.error(request, "Email doesn't exist in the database. Try again or register.")
+        return redirect('/')
+
+# tried to do validations in models.py, but couldn't get it to work (errors2 dict comes back as NoneType)
+# def login(request):
+#     errors2 = User.objects.validate_login(request.POST)
+#     if len(errors2) > 0:
+#         for key, value in errors.items():
+#             messages.error(request, value)
+#         return redirect('/')
+#     else:
+#         # logged_user = User.objects.get(email=request.session['email'])  # or .filter
+#         # request.session['user_id'] = logged_user.id
+#         return redirect('/success')
+
 def success(request):
-    return render(request, 'success.html')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user
+    }
+    return render(request, 'success.html', context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
