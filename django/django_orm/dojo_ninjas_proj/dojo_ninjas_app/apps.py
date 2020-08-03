7e3252d13eccc582e5491a9c6032e3128e127f8b
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-02 22:09:50 -0700

    moved text file to project

--- django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DojoNinjasAppConfig(AppConfig):
-    name = 'dojo_ninjas_app'
--- django/django_orm/dojo_ninjas_proj/dojo_ninjas_proj/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('dojo_ninjas_app.urls'))
-]
